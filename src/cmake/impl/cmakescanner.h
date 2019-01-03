e56432b29229bc0786ac4dcbf9707ccd06d2ede6
Fredrik Andersson <11538533+fredsson@users.noreply.github.com>
2019-01-03 21:18:09 +0100

    Added cmake scanner

---
+++ src/cmake/impl/cmakescanner.h
@@ -0,0 +1,49 @@
+#ifndef CMAKE_CMAKESCANNER_H
+#define CMAKE_CMAKESCANNER_H
+#include <fstream>
+#include <memory>
+#include <string>
+
+namespace cmake {
+
+enum TokenType {
+  NONE,
+  SPACE,
+  NEWLINE,
+  IDENTIFIER,
+  PARENLEFT,
+  PARENRIGHT,
+  ARGUMENTUNQUOTED,
+  ARGUMENTQUOTED,
+  ARGUMENTBRACKET,
+  COMMENTBRACKET,
+  BADCHARACTER,
+  BADBRACKET,
+  BADSTRING,
+  ENDOFFILE,
+};
+
+struct Token {
+  TokenType type;
+  std::string text;
+  unsigned int length;
+  unsigned int line;
+  unsigned int column;
+};
+
+class CmakeScanner {
+public:
+  CmakeScanner(const std::string& filePath);
+  ~CmakeScanner();
+  Token getNextToken();
+private:
+  unsigned int currentLine_;
+  unsigned int currentColumn_;
+  char lastChar_;
+  bool scanningArguments_;
+  std::ifstream file_;
+};
+
+}
+
+#endif
